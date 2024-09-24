024ba8d19ad2b929731e6dce5495d8b759274b9c
Zablon Samba <zablonsamba@gmail.com>
2024-09-24 22:22:19 +0300

    function that returns the perimeter of the island described in grid

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,7 +8,8 @@ def island_perimeter(grid):
     Calculate the perimeter of the island described in the grid.
 
     Parameters:
-    grid (list of list of int): A 2D grid where 0 represents water and 1 represents land.
+    grid (list of list of int): A 2D grid where 0 represents water and
+    1 represents land.
 
     Returns:
     int: The perimeter of the island. The perimeter is calculated by checking
